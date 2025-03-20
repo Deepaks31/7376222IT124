dcdbb18715372b31b5a1f002eb4c496c5cdb213b
deepak31 <deepak.it22@bitsathy.ac.in>
2025-03-20 11:51:18 +0530

    created UI

--- Question-1/Average_calculator/calculator/views.py
+++ Question-1/Average_calculator/calculator/views.py
@@ -1,13 +1,14 @@
-from django.http import JsonResponse
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def calculator(request):
+    access_token = request.GET.get('settings.ACCESS_TOKEN')
     num1 = request.GET.get('num1')
     num2 = request.GET.get('num2')
 
     if num1 is not None and num2 is not None:
         try:
-            average = (float(num1) + float(num2)) / 2  
+            average = (float(num1) + float(num2)) / 2 
             return JsonResponse({'average': average})
         except ValueError:
             return JsonResponse({'error': 'Invalid input. Please provide valid numbers.'}, status=400)
